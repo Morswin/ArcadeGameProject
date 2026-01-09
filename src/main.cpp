1a15828a67108cd8cbd0d83a617f936d33a4dc78
Morswin <41349347+Morswin@users.noreply.github.com>
2026-01-09 03:03:49 +0100

    Add MovementInput struct for keyboard movement handling

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,36 @@
 #include "renderer/texture.h"
 #include "sdl_error.h"
 #include "game.h"
+#include "glm/vec2.hpp"
+
+struct MovementInput {
+    bool right = false;
+    bool left = false;
+    bool up = false;
+    bool down = false;
+    glm::vec2 movement{0.0f, 0.0f};
+
+    void UpdateMovement() {
+        float x = 0.0f;
+        float y = 0.0f;
+
+        if (right && !left) {
+            x = 1.0f;
+        }
+        else if (!right && left) {
+            x = -1.0f;
+        }
+
+        if (up && !down) {
+            y = 1.0f;
+        }
+        else if (!up && down) {
+            y = -1.0f;
+        }
+
+        movement = glm::vec2{x, y};
+    }
+};
 
 // TODO - This needs to get moved/abstracted into a separate thing. Maybe game.h would do? Or something else.
 static Game* game = nullptr;
@@ -22,8 +52,10 @@ static VertexArray* va = nullptr;
 static Shader* shader = nullptr;
 static Renderer* renderer = nullptr;
 static Texture* texture = nullptr;
+static MovementInput movement_input;
 static unsigned int fps = 0;
 static unsigned int lastDeltaTime = 0;
+
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
@@ -94,6 +126,38 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
     case SDL_EVENT_WINDOW_RESIZED:
         GLCall(glViewport(0, 0, event->window.data1, event->window.data2));
         break;
+    case SDL_EVENT_KEY_DOWN:
+        switch (event->key.key) {
+        case SDLK_W:
+            movement_input.up = true;
+            break;
+        case SDLK_A:
+            movement_input.left = true;
+            break;
+        case SDLK_S:
+            movement_input.down = true;
+            break;
+        case SDLK_D:
+            movement_input.right = true;
+            break;
+        }
+        break;
+    case SDL_EVENT_KEY_UP:
+        switch (event->key.key) {
+        case SDLK_W:
+            movement_input.up = false;
+            break;
+        case SDLK_A:
+            movement_input.left = false;
+            break;
+        case SDLK_S:
+            movement_input.down = false;
+            break;
+        case SDLK_D:
+            movement_input.right = false;
+            break;
+        }
+        break;
     }
     // For now I don't see a reason to handle the default.
     // I only want to handle specific events. Not all of them.
@@ -103,6 +167,10 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
     unsigned int tickStartTime = SDL_GetTicks();
+
+    movement_input.UpdateMovement();
+    // SDL_Log("Movement: (%.2f, %.2f)", movement_input.movement.x, movement_input.movement.y);
+    // IT WORKS :)
 
     renderer->Clear();
     renderer->Draw(*va, *eb, *shader);
