2e54d6375245f92ff1ae8ac0254fc6719bfd2e78
Morswin <41349347+Morswin@users.noreply.github.com>
2026-01-12 03:47:18 +0100

    Add projection to the rendering
    Now things look smaller in the window

--- shaders/basic.glsl
+++ shaders/basic.glsl
@@ -7,10 +7,11 @@ layout (location = 1) in vec2 a_TexCoord;
 out vec2 v_TexCoord;
 
 uniform mat4 u_Model;
+uniform mat4 u_MVP;  // Model View Projection
 
 void main()
 {
-    gl_Position = u_Model * vec4(a_Position, 1.0);
+    gl_Position = u_Model * u_MVP * vec4(a_Position, 1.0);
     v_TexCoord = a_TexCoord;
 }
 
--- src/renderer/render_mesh.hpp
+++ src/renderer/render_mesh.hpp
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <SDL3/SDL.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include "vertex_array.hpp"
 #include "vertex_buffer.hpp"
 #include "vertex_buffer_layout.hpp"
@@ -32,6 +34,8 @@ struct RenderMesh {
         texture = new Texture(actualTexPath);
         texture->Bind();
         shader->SetUniform1i("u_Texture", 0);
+        glm::mat4 projection = glm::ortho(-10.0f, 10.0f, -7.5f, 7.5f, -1.0f, 1.0f);  // This could be declared somewhere else, but I beleive that haveing 1 consistent projection for all elements in this game will be good enough. I will leave it hard coded here. Sorry if that upsets you.
+        shader->SetUniform1mat4("u_MVP", projection);
         vao->Unbind();
         vbo->Unbind();
         ebo->Unbind();
